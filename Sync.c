a9374f5e96e1a84ce01a3a579ac0624c5f20d5f5
ada-rsh-s <aadarsh04ss@gmail.com>
2024-07-06 11:04:37 +0530

    Sync.c

--- Sync.c
+++ Sync.c
@@ -1,47 +1,45 @@
 #include<stdio.h>
 #include<semaphore.h>
+#include<pthread.h>
+#include<unistd.h>
 
-int shared=0;
+int shared=1;
 sem_t sem;
 
 void* thread1(){
     sem_wait(&sem);
     int x=shared;
-    printf("The thread2 accessed the shared variable as %d",x);
+    printf("The thread1 accessed the shared variable as %d\n",x);
     x++;
-    printf("The thread2 locally updated the shared variable as %d",x);
+    printf("The thread1 locally updated the shared variable as %d\n",x);
+    sleep(1);
     shared=x;
-    printf("The thread2 updating the shared variable as %d",shared);
+    printf("The thread1 updating the shared variable as %d\n",shared);
     sem_post(&sem);
 }
 
 void* thread2(){
     sem_wait(&sem);
     int x=shared;
-    printf("The thread2 accessed the shared variable as %d",x);
+    printf("The thread2 accessed the shared variable as %d\n",x);
     x--;
-    printf("The thread2 locally updated the shared variable as %d",x);
+    printf("The thread2 locally updated the shared variable as %d\n",x);
+    sleep(1);
     shared=x;
-    printf("The thread2 updating the shared variable as %d",shared);
+    printf("The thread2 updating the shared variable as %d\n",shared);
     sem_post(&sem);
 }
 
 int main(){
-    pthread_t t1[10],t2[10];
-    sem_init(&sem,0);
+    pthread_t t1,t2;
+    sem_init(&sem,0,1);
     
-    for(int i=0;i<5;i++){
-        pthread_create(&t1[i],NULL,(void*) thread1,NULL);
-    }
-    for(int i=0;i<5;i++){
-        pthread_create(&t2[i],NULL,(void*) thread2,NULL);
-    }
-    for(int i=0;i<5;i++){
-        pthread_join(t1[i],NULL);
-    }
-    for(int i=0;i<5;i++){
-        pthread_join(t1[i],NULL);
-    }
-    printf("The final value of shared variable is:");
+    pthread_create(&t1,NULL,(void*) thread1,NULL);
+    pthread_create(&t2,NULL,(void*) thread2,NULL);
+    
+    pthread_join(t1,NULL);
+    pthread_join(t2,NULL);
+    
+    printf("The final value of shared variable is : %d",shared);
     
 }
